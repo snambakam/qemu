598db1958cb8aa34f50701950c67193e0a4cc814
Joerg Roedel <joerg.roedel@amd.com>
2025-08-14 14:45:49 +0200

    hw/core/machine: Add device-plane property
    Add a property to the QEMU MachineState to specify the default plane
    to send device interrupts to.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- include/hw/core/boards.h
+++ include/hw/core/boards.h
@@ -454,6 +454,9 @@ struct MachineState {
      * Set to false by default for all regular use.
      */
     bool new_accel_vmfd_on_reset;
+
+    /* Default plane to receive device IRQs */
+    uint8_t device_plane;
 };
 
 /*
